3f939ca329836f97fd845e11bb3457672fc14573
1198800822 <75421108+1198800822@users.noreply.github.com>
2024-04-30 00:25:44 +0800

    Delete myspring directory

--- myspring/src/main/java/com/sundd/myspring/mapper/OrderMapper.java
+++
@@ -1,7 +0,0 @@
-package com.sundd.myspring.mapper;
-
-import com.sundd.myspring.pojo.Order;
-
-public interface OrderMapper {
-	public Order get(int id);
-}
--- myspring/src/main/java/com/sundd/myspring/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.sundd.myspring.service;
-
-import com.sundd.myspring.pojo.Order;
-
-public interface OrderService {
-	public Order get(int id);
-
-}
